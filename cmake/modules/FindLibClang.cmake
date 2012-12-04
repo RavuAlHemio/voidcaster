9f0a962eb6a03bedd6d2433c3a0094027c5e6860
Ondřej Hošek <ondra.hosek@gmail.com>
2012-12-04 23:34:38 +0100

    let llvm-config give us an include path hint too

--- cmake/modules/FindLibClang.cmake
+++ cmake/modules/FindLibClang.cmake
@@ -14,10 +14,15 @@ if(LLVM_CONFIG)
 		OUTPUT_VARIABLE _llvm_lib_dir
 		OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
+	execute_process(
+		COMMAND ${LLVM_CONFIG} --includedir
+		OUTPUT_VARIABLE _llvm_include_dir
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
 endif(LLVM_CONFIG)
 
 # find the headers
-find_path(LIBCLANG_INCLUDE_DIR clang-c/Index.h)
+find_path(LIBCLANG_INCLUDE_DIR clang-c/Index.h HINTS ${_llvm_include_dir})
 
 # find the library
 find_library(LIBCLANG_LIBRARY NAMES clang libclang liblibclang HINTS ${_llvm_lib_dir})
